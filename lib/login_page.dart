5fdd75096853dc42abc62921b3862d1607111c89
recen <recentnafimahmed@gmail.com>
2023-04-16 10:56:53 +0600

    Nafim login page done 16/04/2023

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
 
 class LoginPage extends StatelessWidget
 {
@@ -13,77 +14,127 @@ class LoginPage extends StatelessWidget
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
+      body: Container(
 
 
-          Padding(
-            padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
-            child:TextField(
-              maxLines: 1,
-              controller: emailController,
-              decoration: InputDecoration(
-                  labelText: "Email",
-                  labelStyle: GoogleFonts.raleway(
+        decoration: BoxDecoration(
 
-                  ),
-                  floatingLabelStyle: GoogleFonts.raleway(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.pink,
+          image: DecorationImage(
+            image: AssetImage("assets/window_view.jpg"),
+            fit: BoxFit.cover
+          )
 
-                  ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30)
-                  ),
-                  filled: true,
-                  fillColor: Colors.white70,
-                  hintText: "Email",
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                          color: Colors.pink,
-                          width: 1.0
-                      )
-                  )
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+
+            Padding(
+              padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+              child:TextField(
+                maxLines: 1,
+                controller: emailController,
+                decoration: InputDecoration(
+                    labelText: "Email",
+                    labelStyle: GoogleFonts.raleway(
+
+                    ),
+                    floatingLabelStyle: GoogleFonts.raleway(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.pink,
+
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30)
+                    ),
+                    filled: true,
+                    fillColor: Colors.white70,
+                    hintText: "Email",
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                            color: Colors.pink,
+                            width: 1.0
+                        )
+                    )
+                ),
+              ),),
+
+
+            Padding(
+              padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+              child:TextField(
+                maxLines: 1,
+                controller: passwordController,
+                decoration: InputDecoration(
+
+                    labelStyle: GoogleFonts.raleway(
+
+                    ),
+                    floatingLabelStyle: GoogleFonts.raleway(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.pink,
+
+                    ),
+                    labelText: "Password",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30)
+                    ),
+                    filled: true,
+                    fillColor: Colors.white70,
+                    hintText: "Password",
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                            color: Colors.pink,
+                            width: 1.0
+                        )
+                    )
+                ),
+              ),),
+
+
+
+
+            ElevatedButton(
+              onPressed: () {
+
+               // Navigator.pop(context);
+
+                // Navigator.push(
+                //     context,
+                //     MaterialPageRoute(
+                //         builder: (context) =>
+                //             BottomBar()));
+
+                // Get.to(BottomBar(),
+                //     duration: Duration(milliseconds: 500), //duration of transitions, default 1 sec
+                //     transition: Transition.rightToLeft );
+
+
+
+
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(
+                    horizontal: 32.w, vertical: 2.h),
+                backgroundColor: Colors.pink,
+                shape: StadiumBorder(),
               ),
-            ),),
-
-
-          Padding(
-            padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
-            child:TextField(
-              maxLines: 1,
-              controller: passwordController,
-              decoration: InputDecoration(
-
-                  labelStyle: GoogleFonts.raleway(
-
-                  ),
-                  floatingLabelStyle: GoogleFonts.raleway(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.pink,
-
-                  ),
-                  labelText: "Password",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30)
-                  ),
-                  filled: true,
-                  fillColor: Colors.white70,
-                  hintText: "Password",
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                          color: Colors.pink,
-                          width: 1.0
-                      )
-                  )
+              child: Text(
+                "Log in",
+                style: TextStyle(color: Colors.white, fontSize: 18,
+                    fontWeight: FontWeight.bold
+                ),
               ),
-            ),),
+            ),
+
+
+            SizedBox(height: 5.h,),
 
 
 
@@ -92,7 +143,53 @@ class LoginPage extends StatelessWidget
 
 
 
-        ],
+
+
+            InkWell(
+              onTap: (){
+
+              },
+              child:  Text("Forgot password ?",
+                style: GoogleFonts.raleway(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.pink
+                ),
+              ),
+            ),
+
+
+            SizedBox(height: 5.h,),
+
+
+
+
+            InkWell(
+              onTap: (){
+
+
+                // Navigator.push(
+                //     context,
+                //     MaterialPageRoute(
+                //         builder: (context) =>
+                //             RegistrationScreen()));
+
+                // Get.to(RegistrationScreen());
+
+
+              },
+              child:  Text("No Account ? Click here to register",
+                style: GoogleFonts.raleway(
+
+                    fontSize: 20,
+                    color: Colors.pink
+                ),
+              ),
+            )
+
+
+          ],
+        ),
       ),
     );
   }
