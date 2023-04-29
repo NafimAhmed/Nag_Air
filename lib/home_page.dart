ec23fad00cd9941638c5aac12b3de9786568d769
recen <recentnafimahmed@gmail.com>
2023-04-29 18:39:45 +0600

    Nafim all booking page is on going  06:39pm 29/04/2023

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -18,68 +18,108 @@ class HomePage extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
 
-          SizedBox(height: 50.sp,),
-
-
-          Row(
-            children: [
-              Column(
-                children: [
-                  Text("Good Morning"),
-                  Text("Book Ticket")
-                ],
-              )
-            ],
-          ),
+            SizedBox(height: 50.sp,),
 
 
-
-          ElevatedButton(onPressed:() {
-            // Navigator.pop(context);
-
-            Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        BottomBar(
-                          initindx: 1,
-                        )));
-
-          }, child: Text("Book")),
-
-
-
-
-
-
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 20,horizontal: 10),
-            height: 20.h,
-            width: 100.h,
-            child: Row(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Expanded(
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 15,
-                    itemBuilder: (BuildContext context, int index) => Card(
-                      child: Center(child: Text('Dummy Card Text')
 
+                Column(
+                  children: [
+
+                    Text("Good morning",
+                        style: TextStyle(fontSize: 17,color: Colors.grey.shade500,fontWeight: FontWeight.bold)
+                    ),
+
+                    SizedBox(width: 5.sp,),
+
+                    Text("Book Tickets",
+                        style: TextStyle(fontSize: 26,color: Color(0xFF3b3b3b),fontWeight: FontWeight.bold)
+                    ),
+
+                  ],
+                ),
+
+
+
+
+
+                Container(
+                  child: Image.asset("assets/nag-air-logo.png",
+                    height: 50,
+                    width: 170,
+                  ),
+                )
+
+
+              ],
+            ),
+
+
+            InkWell(onTap:() {
+              // Navigator.pop(context);
+
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          BottomBar(
+                            initindx: 1,
+                          )));
+
+            }, child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white
+
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 12),
+                child: Row(
+                  children: [
+                    Icon(Icons.search),
+
+                    Text("Search for booking")
+                  ],
+                )
+            ),),
+
+
+
+
+
+
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20,horizontal: 0),
+              height: 20.h,
+              width: 100.h,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 15,
+                      itemBuilder: (BuildContext context, int index) => Card(
+                        child: Center(child: Text('Dummy Card Text')
+
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
 
 
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/search_page.dart
+++ lib/search_page.dart
@@ -2,18 +2,228 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
-class SearchPage extends StatelessWidget{
+// class SearchPage extends StatefulWidget{
+//
+//
+//   @override
+//   State<SearchPage> createState() => _SearchPageState();
+// }
+
+class SearchPage extends StatelessWidget {
+   List<String> listDeparature = <String>['Deparature', 'Dhaka', 'Rajshahi', 'Jassor'];
+
+   List<String> listArrival = <String>['Arrival', 'Dhaka', 'Rajshahi', 'Jassor'];
+
+   RxString dropdownValueDeparature = 'Deparature'.obs;
+   RxString dropdownValueArrival = 'Arrival'.obs;
+
+
+
   @override
   Widget build(BuildContext context) {
+
+
     // TODO: implement build
     return Scaffold(
-      body: Column(
-        children: [
+      body: Obx(()=>Container(
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
 
-        ],
+            SizedBox(height: 40,),
+            // Gap(AppLayout.getHeight(40)),
+            Text("What are \nYou Looking for ?",
+              style: TextStyle(fontSize: 30.sp,color: Colors.black,fontWeight: FontWeight.bold),
+            ),
+            //Gap(AppLayout.getHeight(20)),
+
+            SizedBox(height: 20,),
+
+
+
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 12.sp,horizontal: 12.sp),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12.sp),
+                  color: Colors.white
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.flight_takeoff_rounded,
+                    //Icons.flight_takeoff_rounded,
+                    color: Colors.pink,
+                  ),
+                  // Gap(AppLayout.getWidth(10)),
+
+                  //Text(text,style: Styles.textStyle,)
+
+                  SizedBox(width: 10,),
+
+                  //////////////////////////////////////////////////////////////////////////////
+                  DropdownButton<String>(
+                    value: dropdownValueDeparature.value,
+                    icon: const Icon(Icons.arrow_drop_down),
+                    elevation: 16,
+                    style:  GoogleFonts.openSans(color: Colors.black,
+                        fontSize: 18
+
+                    ),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.transparent,
+                    ),
+                    onChanged: (String? value) {
+                      // This is called when the user selects an item.
+                      //setState(() {
+                        dropdownValueDeparature.value = value!;
+                      //});
+                    },
+                    items: listDeparature.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                  ///////////////////////////////////////////////////////
+
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 12.sp,horizontal: 12.sp),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12.sp),
+                  color: Colors.white
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.flight_land_rounded,
+                    //Icons.flight_takeoff_rounded,
+                    color: Colors.pink,
+                  ),
+                  // Gap(AppLayout.getWidth(10)),
+
+                  //Text(text,style: Styles.textStyle,)
+
+                  SizedBox(width: 10,),
+
+                  //////////////////////////////////////////////////////////////////////////////
+
+
+
+
+                  DropdownButton<String>(
+                    value: dropdownValueArrival.value,
+                    icon: const Icon(Icons.arrow_drop_down),
+                    elevation: 16,
+                    style:GoogleFonts.openSans(color: Colors.black,
+                        fontSize: 18
+
+                    ),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.transparent,
+                    ),
+                    onChanged: (String? value) {
+                      // This is called when the user selects an item.
+                     // setState(() {
+                        dropdownValueArrival.value = value!;
+                     // });
+                    },
+                    items: listArrival.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+
+
+                  ///////////////////////////////////////////////////////
+
+                ],
+              ),
+            ),
+
+
+            SizedBox(height: 20,),
+
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 12.sp,horizontal: 12.sp),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12.sp),
+                  color: Colors.white
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.calendar_month,
+                    //Icons.flight_takeoff_rounded,
+                    color: Colors.pink,
+                  ),
+                  // Gap(AppLayout.getWidth(10)),
+
+                  //Text(text,style: Styles.textStyle,)
+
+                  SizedBox(width: 10,),
+
+                  //////////////////////////////////////////////////////////////////////////////
+
+
+
+
+                  InkWell(
+                    onTap: (){
+                      // SfDateRangePicker(
+                      //   onSelectionChanged: (arg){
+                      //
+                      //   },
+                      //   selectionMode: DateRangePickerSelectionMode.single,
+                      //   initialSelectedRange: PickerDateRange(
+                      //       DateTime.now().subtract(const Duration(days: 4)),
+                      //       DateTime.now().add(const Duration(days: 3))),
+                      // );
+                    },
+                    child: Text("Select journey date",
+
+                      style:GoogleFonts.openSans(color: Colors.black,
+                          fontSize: 15
+
+                      ),
+
+                    ),
+                  )
+
+
+
+                  ///////////////////////////////////////////////////////
+
+                ],
+              ),
+            ),
+
+
+
+
+
+
+
+
+          ],
+        ),
       ),
+      )
+
     );
   }
-
 }
